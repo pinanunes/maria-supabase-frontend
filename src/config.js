adae88f00571adafe2a1b9790dd3601a45d26f4a
Telmo Nunes <pinanunes@gmail.com>
2025-08-15 16:14:53 +0100

    Update webhook addresses

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuração dos webhooks
 export const WEBHOOKS = {
-  CHAT: 'http://manuelnunes.duckdns.org:5678/webhook/0617abad-ef75-4320-8231-1e4468ce6a83',
-  FEEDBACK: 'http://manuelnunes.duckdns.org:5678/webhook/429f2428-e083-4d60-84a1-610d4808b0a3'
+  CHAT: 'https://manuelnunes.duckdns.org:5678/webhook/0617abad-ef75-4320-8231-1e4468ce6a83',
+  FEEDBACK: 'https://manuelnunes.duckdns.org:5678/webhook/429f2428-e083-4d60-84a1-610d4808b0a3'
 };
 
 // Outras configurações da aplicação
